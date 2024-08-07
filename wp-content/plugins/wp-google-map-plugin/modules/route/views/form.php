bb3d59187b8af15aa758ec86476aafc47ca6ca93
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-08-07 16:24:58 +0800

    deleted

--- wp-content/plugins/wp-google-map-plugin/modules/route/views/form.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-include_once(WPGMP_DIR . 'inc/modals/pro.php'); 
-$add_route_image = WPGMP_PRO_IMAGES."/add-Route.png";
-echo "<div class='fc_map_page_block'><img src='".esc_url($add_route_image)."' alt='add-Route'></div>";
